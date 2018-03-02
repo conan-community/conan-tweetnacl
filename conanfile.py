f2e94633471dc85a7da29cc03587648240ec497b
Pedro Vicente <pedrovfer@gmail.com>
2018-03-02 15:05:43 +0000

    Addressed review changes

--- conanfile.py
+++ conanfile.py
@@ -5,29 +5,34 @@ class TweetnaclConan(ConanFile):
     name = "tweetnacl"
     version = "20140427"
     license = "Public Domain"
-    homepage = "https://tweetnacl.cr.yp.to/"
+    homepage = "https://tweetnacl.cr.yp.to"
     url = "https://github.com/conan-community/conan-tweetnacl"
     description = "TweetNaCl is the world's first auditable high-security cryptographic library"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    exports_sources = "src/*"
     build_policy = "missing"
+    exports = ["PUBLIC_DOMAIN_LICENSE.md", "LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
 
     def configure(self):
         del self.settings.compiler.libcxx
 
+    def source(self):
+        url = "%s/%s/" % (self.homepage, self.version)
+        sources = ["tweetnacl.h", "tweetnacl.c"]
+        for s in sources:
+            tools.download(url+s, s)
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.h", dst="include")
         self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        self.copy(pattern="*LICENSE*", dst="licenses")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
